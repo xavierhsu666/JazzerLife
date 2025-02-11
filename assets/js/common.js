289cd83c476288b4b5ab4dd50d1325a66916e71a
Kazuo <wryi636@gmail.com>
2025-02-11 19:14:23 +0800

    Delete assets directory

--- assets/js/common.js
+++
@@ -1,7 +0,0 @@
-// ! -- Global Area ----------------------------------------------------------------------
-var car_Vehicles_DBName = "[ME_Planning].[OMT].[DX_JZ_Vehicles]";
-var car_OilCon_DBName = "[ME_Planning].[OMT].[DX_JZ_FuelConsumption]";
-var mem_User_DBName = "[ME_Planning].[OMT].[DX_JZ_Users]";
-var s = location.pathname;
-var t = (url = location.href);
-sourcePage = t.split("#")[1];
